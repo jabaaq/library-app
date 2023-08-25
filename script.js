10bac7a21178c335c8cd759c90257b7e1f611b8b
Jaba <jabaqadagishvili4@gmail.com>
2023-08-25 23:59:09 +0400

    Modal added

--- script.js
+++ script.js
@@ -1,9 +1,51 @@
 'use strict';
 
+const bookTitle = document.querySelector('titleInput'),
+    bookAuthor = document.querySelector('authorInput'),
+    bookPages = document.querySelector('pageNumber'),
+    addBookbtn = document.querySelector('.add-book-btn'),
+    modal = document.querySelector('.modal'),
+    modalCloseBtn = document.querySelector('#close-modal-btn')
+
+
+function showModal() {
+    modal.classList.add('open')
+}
+
+function closeModal() {
+    modal.classList.remove('open')
+}
+
+addBookbtn.addEventListener('click', () => {
+    showModal();
+})
+
+window.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape') {
+        closeModal();
+    }
+})
+
+window.addEventListener('click', () => {
+    if (event.target == modal) {
+        closeModal();
+    }
+})
+
+
+modalCloseBtn.addEventListener('click', () => {
+    closeModal();
+})
+
+
+
+
 const myLibrary = [];
 
-function book() {
-
+function book(title, author, pages) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
 }
 
 function addBookToLibrary() {
